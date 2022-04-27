987ab53644f14070e590c038fb9d601cf2dbd96e
CarolynnFleming <cfcatpink@gmail.com>
2022-04-27 15:48:48 -0700

    added name

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,7 +14,7 @@ const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: '',
+  name: 'Saske',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
