5ee5dd7bf5deed711e7ed2c89573d8d41493efe5
CarolynnFleming <cfcatpink@gmail.com>
2022-04-28 09:05:37 -0700

    uncommented

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -54,23 +54,23 @@ test('Should render the header', async () => {
   expect(profileName).toBeInTheDocument()
 })
 
-test('Should render the header with Sasuke 🌬️🔥', async () => {
-  const sasuke = {
-    id: 1,
-    created_at: '2021-12-13T00:17:29+00:00',
-    name: 'Sasuke 🌬️🔥',
-    avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
-    header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
-    likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
-    motto: 'Res Non Verba',
-    color: 'crimson',
-  }
+// test('Should render the header with Sasuke 🌬️🔥', async () => {
+//   const sasuke = {
+//     id: 1,
+//     created_at: '2021-12-13T00:17:29+00:00',
+//     name: 'Sasuke 🌬️🔥',
+//     avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+//     header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+//     likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+//     motto: 'Res Non Verba',
+//     color: 'crimson',
+//   }
 
-  // 🚨 Use the server to change the response for this test
+//   // 🚨 Use the server to change the response for this test
 
-  render(<App />)
+//   render(<App />)
 
-  const profileName = await screen.findByText(sasuke.name)
+//   const profileName = await screen.findByText(sasuke.name)
 
-  expect(profileName).toBeInTheDocument()
-})
+//   expect(profileName).toBeInTheDocument()
+// })
