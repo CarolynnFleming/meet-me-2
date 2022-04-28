16f48d2c52e389a925a9d7ee2c504699f4fb8bbc
CarolynnFleming <cfcatpink@gmail.com>
2022-04-28 08:56:11 -0700

    added square brackets to user

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,7 +14,7 @@ const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: 'Saske',
+  name: 'naruto',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -24,8 +24,11 @@ const user = {
 
 // 🚨 Create your server
 const server = setupServer(
-  rest.get(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/users?select=*`,(req, res, ctx) => {
-    return res(ctx.json(user))
+  rest.get(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/users`,(req, res, ctx) => {
+    // ?select=*
+    // req.url.searchParams
+    const select = req.url.searchParams.get('select')
+    return res(ctx.json([user]))
   })
 )
 // 🚨 Listen for server start
