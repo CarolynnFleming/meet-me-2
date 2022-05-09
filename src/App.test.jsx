9afb4961958d2e43209df3c06ab310f327a20887
CarolynnFleming <cfcatpink@gmail.com>
2022-05-09 10:42:04 -0700

    used server to change response for test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -54,23 +54,28 @@ test('Should render the header', async () => {
   expect(profileName).toBeInTheDocument()
 })
 
-// test('Should render the header with Sasuke 🌬️🔥', async () => {
-//   const sasuke = {
-//     id: 1,
-//     created_at: '2021-12-13T00:17:29+00:00',
-//     name: 'Sasuke 🌬️🔥',
-//     avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
-//     header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
-//     likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
-//     motto: 'Res Non Verba',
-//     color: 'crimson',
-//   }
+test('Should render the header with Sasuke 🌬️🔥', async () => {
+  const sasuke = {
+    id: 1,
+    created_at: '2021-12-13T00:17:29+00:00',
+    name: 'Sasuke 🌬️🔥',
+    avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+    header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+    likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+    motto: 'Res Non Verba',
+    color: 'crimson',
+  }
 
-//   // 🚨 Use the server to change the response for this test
+  // // 🚨 Use the server to change the response for this test
+  server.use(
+    rest.get(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/users`,(req, res, ctx) => {
+      return res(ctx.json([sasuke]))
+    }));
 
-//   render(<App />)
+    
+  render(<App />)
 
-//   const profileName = await screen.findByText(sasuke.name)
+  const profileName = await screen.findByText(sasuke.name)
 
-//   expect(profileName).toBeInTheDocument()
-// })
+  expect(profileName).toBeInTheDocument()
+})
